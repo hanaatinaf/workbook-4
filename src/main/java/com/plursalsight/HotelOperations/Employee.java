112f9d1d4db7a53d6fe5aee8f228016170f6a3ba
Hana Atinaf <hanaatianf2@gmail.com>
2025-10-20 15:11:01 -0600

    working on employee class

---
+++ src/main/java/com/plursalsight/HotelOperations/Employee.java
@@ -0,0 +1,68 @@
+package com.plursalsight.HotelOperations;
+
+public class Employee {
+    private  int employeeId;
+    private String name;
+    private  String department;
+    private  double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, double payRate, String department, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.payRate = payRate;
+        this.department = department;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public double getTotalPay(){
+
+        return 0;
+    }
+    public  double getRegularHours(){
+
+        return 0;
+    }
+    public  double getOvertimeHours(){
+        return 0;
+    }
+}
